1a6546d1fde017c82c8c1ee5f3ca7faca4cea3b6
Justin Lee Edwards <justin@highdefdesigns.com>
2024-05-17 22:12:21 -0500

    put request

--- routes/posts.js
+++ routes/posts.js
@@ -46,4 +46,19 @@ router.post('/', (req, res) => {
   res.status(201).json(posts);
 });
 
+// PUT
+router.put('/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    return res
+      .status(400)
+      .json({ message: `A post with the id of ${id} was not found` });
+  }
+
+  post.title = req.body.title;
+  res.status(200).json(posts);
+});
+
 export default router;
