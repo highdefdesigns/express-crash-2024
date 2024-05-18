95cd53549b0636ac68e09ae03f6229204a330092
Justin Lee Edwards <justin@highdefdesigns.com>
2024-05-17 22:45:01 -0500

    delete request

--- routes/posts.js
+++ routes/posts.js
@@ -61,4 +61,19 @@ router.put('/:id', (req, res) => {
   res.status(200).json(posts);
 });
 
+// DELETE a post
+router.delete('/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    return res
+      .status(400)
+      .json({ message: `A post with the id of ${id} was not found` });
+  }
+
+  posts = posts.filter((post) => post.id !== id);
+  res.status(200).json(posts);
+});
+
 export default router;
