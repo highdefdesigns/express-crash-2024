2bb8b1166be9bfb48714976247795eca2ef01407
Justin Lee Edwards <justin@highdefdesigns.com>
2024-05-24 13:48:58 -0500

    front end request

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import colors from 'colors';
 import posts from './routes/posts.js';
 import logger from './middleware/logger.js';
@@ -7,6 +8,9 @@ import errorHandler from './middleware/error.js';
 import notFound from './middleware/notFound.js';
 const port = process.env.PORT || 8000;
 
+// Get the directory name workaround with type: module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 
 // Body parser middleware
@@ -16,9 +20,8 @@ app.use(express.json());
 // takes care of form data (x-www-form-urlencoded)
 app.use(express.urlencoded({ extended: false }));
 app.use(logger);
-
 // setup static folder
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/api/posts', posts);
