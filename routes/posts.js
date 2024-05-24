762103eb8d065663154f4caa24d9f47dcc0167fb
Justin Lee Edwards <justin@highdefdesigns.com>
2024-05-24 12:27:38 -0500

    error handling

--- routes/posts.js
+++ routes/posts.js
@@ -8,38 +8,39 @@ let posts = [
 ];
 
 // get all posts
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   const limit = parseInt(req.query.limit);
   //   get a limit on how many posts shown
   if (!isNaN(limit) && limit > 0) {
     return res.status(200).json(posts.slice(0, limit));
   }
-
   res.status(200).json(posts);
 });
 
 // get specific posts
-router.get('/:id', (req, res) => {
+router.get('/:id', (req, res, next) => {
   const id = parseInt(req.params.id);
   const post = posts.find((post) => post.id === id);
 
   if (!post) {
-    return res.status(404).json({
-      message: `A post with the id of ${id} was not found`,
-    });
+    const error = new Error(`A post with the id of ${id} was not found`);
+    error.status = 404;
+    return next(error);
   }
+
   res.status(200).json(post);
 });
 
 // Create new post
-router.post('/', (req, res) => {
+router.post('/', (req, res, next) => {
   const data = {
     id: posts.length + 1,
     title: req.body.title,
   };
-
-  if (!data) {
-    return res.status(400).json({ message: 'Please include a title' });
+  if (!data.title) {
+    const error = new Error('Please include a title');
+    error.status = 400;
+    return next(error);
   }
   posts.push(data);
 
@@ -47,14 +48,14 @@ router.post('/', (req, res) => {
 });
 
 // PUT
-router.put('/:id', (req, res) => {
+router.put('/:id', (req, res, next) => {
   const id = parseInt(req.params.id);
   const post = posts.find((post) => post.id === id);
 
   if (!post) {
-    return res
-      .status(400)
-      .json({ message: `A post with the id of ${id} was not found` });
+    const error = new Error(`A post with the id of ${id} was not found`);
+    error.status = 404;
+    return next(error);
   }
 
   post.title = req.body.title;
@@ -62,14 +63,14 @@ router.put('/:id', (req, res) => {
 });
 
 // DELETE a post
-router.delete('/:id', (req, res) => {
+router.delete('/:id', (req, res, next) => {
   const id = parseInt(req.params.id);
   const post = posts.find((post) => post.id === id);
 
   if (!post) {
-    return res
-      .status(400)
-      .json({ message: `A post with the id of ${id} was not found` });
+    const error = new Error(`A post with the id of ${id} was not found`);
+    error.status = 404;
+    return next(error);
   }
 
   posts = posts.filter((post) => post.id !== id);
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import path from 'path';
 import posts from './routes/posts.js';
 import logger from './middleware/logger.js';
+import errorHandler from './middleware/error.js';
+import notFound from './middleware/notFound.js';
 const port = process.env.PORT || 8000;
 
 const app = express();
@@ -17,6 +19,11 @@ app.use(logger);
 // setup static folder
 // app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.use('/api/posts', posts);
+
+// error handling
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
